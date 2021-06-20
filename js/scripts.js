64b8aebe021be0dabb26ddbb36dce6bc98c26376
Jason Collier <jason.collier@iinet.net.au>
2021-06-20 17:40:14 +0800

    javascript stuff

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
-console.log('test');
+const xmlhttp = new XMLHttpRequest();
+const url = "data.json";
+
+xmlhttp.onreadystatechange = function(){
+    let data = JSON.parse(this.responseText);
+    console.log(data);
+    // myFunction(data);
+};
+xmlhttp.open("GET", url, true);
+xmlhttp.send;
+
+function myFunction(data){
+    console.log('hello');
+    console.log(data[0].company);
+}
